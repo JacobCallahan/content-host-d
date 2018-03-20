27e2fcb063d091a0e49e1ab6f16e650b440642fa
sthirugn <sthirugn@redhat.com>
2018-03-20 16:28:00 -0400

    Added container logs

--- flood.py
+++ flood.py
@@ -29,6 +29,12 @@ def gen_json(hypervisors, guests):
 
 def rm_container(client, containers, reason="Success"):
     del_container = containers[0]
+    with open("container.log", "a") as log:
+        log.write(
+            "***********************************{0}****************************\n".format(
+            del_container['name'])
+        )
+        log.write(client.logs(del_container['container']['Id']))
     client.remove_container(del_container['container'], v=True, force=True)
     del containers[0]
     logging.info('Done with {0}: {1}'.format(del_container['name'], reason))
