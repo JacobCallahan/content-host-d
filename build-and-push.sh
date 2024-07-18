c904201534162d2ade3808a5903af48b70d163ec
omkarkhatavkar <okhatavkar007@gmail.com>
2024-07-18 16:47:47 +0530

    podman support for shell script, we could only have podman

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,13 +2,25 @@ cd ~/content-host-d
 git fetch origin
 git checkout master
 git reset --hard origin/master
+
+# Determine if Docker or Podman is available
+if command -v docker &> /dev/null; then
+    CONTAINER_CMD="docker"
+elif command -v podman &> /dev/null; then
+    CONTAINER_CMD="podman"
+else
+    echo "Neither Docker nor Podman is installed. Exiting."
+    exit 1
+fi
+
 for var in "${@}"
 do
     cp -r resources ~/content-host-d/${var}
     cp -r setup_scripts ~/content-host-d/${var}
-    docker build -t ${var,,} ~/content-host-d/${var}
+    $CONTAINER_CMD build -t ${var,,} ~/content-host-d/${var}
 done
-echo "------------- Cleaning up after docker -------------"
-docker rm -v $(docker ps -q -f "status=exited")
-docker volume rm $(docker volume ls -f dangling=true -q)
-docker rmi $(docker images -f "dangling=true" -q)
+
+echo "------------- Cleaning up after $CONTAINER_CMD -------------"
+$CONTAINER_CMD rm -v $($CONTAINER_CMD ps -q -f "status=exited")
+$CONTAINER_CMD volume rm $($CONTAINER_CMD volume ls -f dangling=true -q)
+$CONTAINER_CMD rmi $($CONTAINER_CMD images -f "dangling=true" -q)
